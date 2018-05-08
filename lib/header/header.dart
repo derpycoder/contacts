a004cd6808701e65ca1cb5313334005d928d4ca3
Abhijit Kar <ak24284@gmail.com>
2018-05-08 22:05:56 +0530

    Header top part finalized

--- lib/header/header.dart
+++ lib/header/header.dart
@@ -5,22 +5,25 @@ class Header extends StatelessWidget {
   AppBar build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.green[300],
-      title: Text("Contacts 2.0"),
+      title: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          color: Colors.white,
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(
+                left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+            border: InputBorder.none,
+            hintText: 'Search Contacts',
+          ),
+        ),
+      ),
       actions: <Widget>[
         IconButton(
-          icon: Icon(Icons.search),
-          tooltip: 'Search',
-          onPressed: () => print("Search"),
-        ),
-        IconButton(
-          icon: Icon(Icons.bookmark),
-          tooltip: 'Bookmarks',
-          onPressed: () => print("Bookmarks"),
-        ),
-        IconButton(
-          icon: Icon(Icons.more_vert),
-          tooltip: 'More',
-          onPressed: () => print("More"),
+          icon: Icon(Icons.share),
+          tooltip: 'Share',
+          onPressed: () => print("Share"),
         ),
       ],
       bottom: TabBar(
