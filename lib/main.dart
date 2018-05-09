32080d14caa4e264754c313f250e9f81a4f5c83c
Abhijit Kar <ak24284@gmail.com>
2018-05-09 09:49:45 +0530

    Added a flexible space sliver

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,15 @@ class MyHomePage extends StatelessWidget {
               child: CustomScrollView(
                 scrollDirection: Axis.vertical,
                 slivers: <Widget>[
+                  SliverAppBar(
+                    leading: Icon(Icons.account_circle),
+                    pinned: true,
+                    expandedHeight: 250.0,
+                    backgroundColor: Colors.pinkAccent,
+                    flexibleSpace: FlexibleSpaceBar(
+                      title: Text('John Doe'),
+                    ),
+                  ),
                   SliverFixedExtentList(
                     itemExtent: 160.0,
                     delegate: SliverChildBuilderDelegate(
