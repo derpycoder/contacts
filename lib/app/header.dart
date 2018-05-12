ebfb4bb94fbe4cefd22baa125dcdfb3034169eb4
Abhijit Kar <ak24284@gmail.com>
2018-05-12 23:03:17 +0530

    Updated the Scaffold

--- lib/app/header.dart
+++ lib/app/header.dart
@@ -2,42 +2,51 @@ import 'package:flutter/material.dart';
 
 import './shared/values/colors.dart';
 
-class Header extends StatelessWidget {
-  @override
-  AppBar build(BuildContext context) {
-    return AppBar(
-      backgroundColor: rawGreen,
-      title: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(30.0)),
-          color: obviousWhite,
-        ),
-        child: TextField(
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.only(
-                left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
-            border: InputBorder.none,
-            hintText: 'Search Contacts',
-          ),
-        ),
+AppBar header() {
+  return AppBar(
+    backgroundColor: rawGreen,
+    title: _searchBar(),
+    actions: <Widget>[
+      _shareContact(),
+    ],
+    bottom: _tabBar(),
+  );
+}
+
+Widget _searchBar() {
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+      color: obviousWhite,
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+        contentPadding:
+            EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+        border: InputBorder.none,
+        hintText: 'Search Contacts',
       ),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.share),
-          tooltip: 'Share',
-          onPressed: () => print("Share"),
-        ),
-      ],
-      bottom: TabBar(
-        indicatorColor: obviousWhite,
-        tabs: <Widget>[
-          Tab(icon: Icon(Icons.account_circle)),
-          Tab(icon: Icon(Icons.favorite)),
-          Tab(icon: Icon(Icons.contacts)),
-          Tab(icon: Icon(Icons.group)),
-          Tab(icon: Icon(Icons.archive)),
-        ],
-      ),
-    );
-  }
+    ),
+  );
+}
+
+Widget _shareContact() {
+  return IconButton(
+    icon: Icon(Icons.share),
+    tooltip: 'Share',
+    onPressed: () => print("Share"),
+  );
+}
+
+Widget _tabBar() {
+  return TabBar(
+    indicatorColor: obviousWhite,
+    tabs: <Widget>[
+      Tab(icon: Icon(Icons.account_circle)),
+      Tab(icon: Icon(Icons.favorite)),
+      Tab(icon: Icon(Icons.contacts)),
+      Tab(icon: Icon(Icons.group)),
+      Tab(icon: Icon(Icons.archive)),
+    ],
+  );
 }
--- lib/app/home-page.dart
+++ lib/app/home-page.dart
@@ -11,25 +11,33 @@ class HomePage extends StatelessWidget {
       length: 5,
       initialIndex: 2,
       child: Scaffold(
-        appBar: Header().build(context),
-        body: SafeArea(
-          child: TabBarView(
-            physics: BouncingScrollPhysics(),
-            children: [
-              Center(child: Text("Profile")),
-              Center(child: Text("Favorites")),
-              ContactsTab(),
-              Center(child: Text("Groups")),
-              Center(child: Text("Trash Can")),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => print("Yay"),
-        ),
+        appBar: header(),
+        body: _tabBody(),
+        floatingActionButton: _addContact(),
         drawer: Sidebar(),
       ),
     );
   }
+}
+
+Widget _tabBody() {
+  return SafeArea(
+    child: TabBarView(
+      physics: BouncingScrollPhysics(),
+      children: [
+        Center(child: Text("Profile")),
+        Center(child: Text("Favorites")),
+        ContactsTab(),
+        Center(child: Text("Groups")),
+        Center(child: Text("Trash Can")),
+      ],
+    ),
+  );
+}
+
+Widget _addContact() {
+  return FloatingActionButton(
+    child: Icon(Icons.add),
+    onPressed: () => print("Yay"),
+  );
 }
